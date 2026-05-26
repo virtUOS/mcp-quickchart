4b0d894b551666ac27596acc47c24410819e6a42
Lars Kiesow <lkiesow@uos.de>
2026-05-26 22:39:56 +0200

    Extract server startup logic into main() function
    This will fix the command defined in the `pyproject.toml`.

--- mcpquickchart/__main__.py
+++ mcpquickchart/__main__.py
@@ -1,10 +1,15 @@
 from mcpquickchart.server import server
 import os
 
-if __name__ == "__main__":
+
+def main():
     port = os.getenv("QUICKCHART_MCP_SERVER_PORT")
     host = os.getenv("QUICKCHART_MCP_SERVER_HOST", "127.0.0.1")
     if port:
         server.run(transport="http", host=host, port=int(port))
     else:
         server.run()
+
+
+if __name__ == "__main__":
+    main()
